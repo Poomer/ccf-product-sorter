7340b4977a5b82d27545b9b29d50dce05cd14b0b
Veerasak Nichayapun <v.nichayapun@gmail.com>
2024-11-19 20:03:31 +0700

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,5 @@
+class constant:
+
+    order_statement_sheetname = 'ใบจัด 011167 Copy (2)'
+    customer_file = '1.ขาย-รับออเดอร์ลูกค้า2567.xlsx'
+    order_statement = '14ใบจัดสินค้า.xlsx'
---
+++ product_sorter.bat
@@ -0,0 +1,3 @@
+python product_sorter.py 
+
+echo "สร้างใบจัดสินค้่าสำเร็จ : Order statement has been created"
